a54039c32807dd4d6042f37816f54d2bc5d77301
gutosie <gutosie@o2.pl>
2020-01-07 11:58:00 +0200

    Update iNB.sh

--- iNB.sh
+++ iNB.sh
@@ -21,8 +21,8 @@ rm -f /tmp/neoboot.zip
 [ $PL ] && echo "Instalowanie..." || echo "Instaling..."
 Cel="/usr/lib/enigma2/python/Plugins/Extensions"
 [ -e $Cel/NeoBoot ] && rm -rf $Cel/NeoBoot/* || mkdir -p $Cel/NeoBoot
-mv -f /tmp/neoboot-master/NeoBoot/* $Cel/NeoBoot
-[ -e /tmp/neoboot-master ] && rm -rf /tmp/neoboot-master
+mv -f /tmp/NeoBoot_VuUltimo4K-master/NeoBoot/* $Cel/NeoBoot
+[ -e /tmp/NeoBoot_VuUltimo4K-master ] && rm -rf /tmp/NeoBoot_VuUltimo4K-master
 cd $Cel/NeoBoot
 chmod 755 ./bin/*
 chmod 755 ./ex_init.py
